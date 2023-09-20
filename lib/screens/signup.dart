5476168af0864bcf8a70498b5696a264247e40cf
Wareesha Khan <72753730+wareesha30@users.noreply.github.com>
2023-09-20 23:57:15 +0500

    new changes

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -6,8 +6,39 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          color: Colors.white, child: Image.asset('assets/images/pattern.png')),
-    );
+        body: Stack(
+      children: [
+        Positioned.fill(child: Image.asset('assets/images/pattern.png')),
+        Positioned(
+            top: 80,
+            right: 100,
+            left: 100,
+            child: Image.asset('assets/images/logo_group.png')),
+        const Positioned(
+            top: 345,
+            right: 50,
+            left: 50,
+            child: Column(
+              children: [
+                Text("Login To Your Account",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w800,
+                    )),
+                SizedBox(
+                  height: 190,
+                ),
+                Text("Or Continue With",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    )),
+                SizedBox(
+                  height: 20,
+                )
+              ],
+            ))
+      ],
+    ));
   }
 }
